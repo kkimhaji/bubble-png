05dc724c3cace2825110a5cdeca1626dbfdbc711
kkimhaji <kkim.haji@gmail.com>
2026-09-24 16:41:29 +0900

    [feat] add JoseonGulim font via CDN @font-face

--- src/fonts.ts
+++ src/fonts.ts
@@ -6,6 +6,12 @@ export interface FontOption {
 
 export const fonts: FontOption[] = [
   { id: 'system', label: '기본 폰트', cssFontFamily: 'sans-serif' },
-  { id: 'chosun-gulim', label: '조선굴림체', cssFontFamily: 'JoseonGulim' },
+  { id: 'joseon-gulim', label: '조선굴림체', cssFontFamily: 'JoseonGulim' },
+  { id: 'nanum-barun-gothic', label: '나눔바른고딕', cssFontFamily: 'NanumBarunGothic'},
+  { id: 'baemin-kkubullim', label: '배달의 민족 꾸불림체', cssFontFamily: 'KkuBulLim'},
+  { id: 'nexon-lv1-gothic', label: '넥슨 Lv1 고딕', cssFontFamily: 'NexonLv1Gothic'},
+  { id: 'galmuri-11', label: '갈무리11', cssFontFamily: 'Galmuri11'},
+
+
   // 폰트를 추가하려면: style.css에 @font-face로 먼저 등록한 뒤, 이 배열에 항목을 추가하세요.
 ];
